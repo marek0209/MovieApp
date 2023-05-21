a2656c4ff2e26bbcd34fcf913eee054ea72ecd05
marek0209 <marek.urbaniec1@gmail.com>
2023-05-21 21:56:27 +0200

    Move Favorite Button to a separate component

---
+++ src/elements/components/favorite-button/FavoriteButton.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface FavoriteButtonProps {
+  isFavorite: boolean;
+  onClick: () => void;
+}
+
+function FavoriteButton(props: FavoriteButtonProps) {
+  const { isFavorite, onClick } = props;
+
+  return (
+    <button
+      className={[
+        "flex-none",
+        "flex",
+        "items-center",
+        "justify-center",
+        "w-9",
+        "h-9",
+        "rounded-md",
+        "text-slate-300",
+        "border",
+        "border-slate-200",
+      ].join(" ")}
+      type="button"
+      aria-label="Like"
+      onClick={onClick}
+    >
+      <svg
+        width="20"
+        height="20"
+        fill={isFavorite ? "red" : "grey"}
+        aria-hidden="true"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+        />
+      </svg>
+    </button>
+  );
+}
+
+export default FavoriteButton;
--- src/elements/components/movie-list-item/MovieListItem.tsx
+++ src/elements/components/movie-list-item/MovieListItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Movie } from "../../../types";
 import resizeImage from "../../../utils/imageResizer";
 import ImageLoader from "../image-loader/ImageLoader";
+import Button from "../favorite-button/FavoriteButton";
 
 interface MovieListItemProps {
   movie: Movie;
@@ -28,36 +29,7 @@ export default function MovieListItem(props: MovieListItemProps) {
       <ImageLoader src={resizedImg} alt={`Cover of ${movie.title.label}`} />
       <h3 className="text-lg font-semibold mt-2">{movie.title.label}</h3>
       <p className="text-gray-700">{movie.category.attributes.label}</p>
-      <button
-        className={[
-          "flex-none",
-          "flex",
-          "items-center",
-          "justify-center",
-          "w-9",
-          "h-9",
-          "rounded-md",
-          "text-slate-300",
-          "border",
-          "border-slate-200",
-        ].join(" ")}
-        type="button"
-        aria-label="Like"
-        onClick={handleFavoriteClick}
-      >
-        <svg
-          width="20"
-          height="20"
-          fill={isFavorite ? "red" : "grey"}
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-          />
-        </svg>
-      </button>
+      <Button isFavorite={isFavorite} onClick={handleFavoriteClick} />
     </div>
   );
 }
