652126887b1cd90d11edce03fd95be0d9333a550
marek0209 <marek.urbaniec1@gmail.com>
2023-05-21 21:03:03 +0200

    Add routing to App

--- src/elements/containers/App.tsx
+++ src/elements/containers/App.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import AppHeader from "../components/app-header/AppHeader";
 import AppFooter from "../components/app-footer/AppFooter";
 import Instructions from "../../scenes/instructions/Instructions";
 import MovieList from "../../scenes/movie-list/MovieList";
+import FavoriteMovies from "../../scenes/favorites/FavoriteMovies";
 import store from "../../redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
+    <Router>
       <div className="App">
         <AppHeader />
         <main>
-          <MovieList />
-          <Instructions />
+          <Provider store={store}>
+            <Routes>
+              <Route path="/movies" element={<MovieList />} />
+              <Route path="/favorites" element={<FavoriteMovies />} />
+              <Route path="/" element={<Instructions />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Provider>
         </main>
         <AppFooter />
       </div>
-    </Provider>
+    </Router>
   );
 }
 
