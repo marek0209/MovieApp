c7303e183f5463fb69c5082c297a9f61a9684e97
marek0209 <marek.urbaniec1@gmail.com>
2023-05-21 20:27:10 +0200

    Add Provider to App container

--- src/elements/containers/App.tsx
+++ src/elements/containers/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Provider } from "react-redux";
 import AppHeader from "../components/app-header/AppHeader";
 import AppFooter from "../components/app-footer/AppFooter";
 import Instructions from "../../scenes/instructions/Instructions";
+import MovieList from "../../scenes/movie-list/MovieList";
+import store from "../../redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <AppHeader />
-      <main>
-        <Instructions />
-      </main>
-      <AppFooter />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppHeader />
+        <main>
+          <MovieList />
+          <Instructions />
+        </main>
+        <AppFooter />
+      </div>
+    </Provider>
   );
 }
 
