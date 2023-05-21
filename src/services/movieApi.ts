6403edc23bd5eb487685239b06f1525ba5a9d7e2
marek0209 <marek.urbaniec1@gmail.com>
2023-05-21 20:07:29 +0200

    Add service for Itunes API

---
+++ src/services/movieApi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { Movie } from "../types";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+export default async function fetchMovieList(): Promise<Movie[]> {
+  try {
+    const response = await instance.get("/topmovies/limit=100/json");
+    const movieList = response.data.feed.entry;
+    return movieList;
+  } catch (error) {
+    console.error("Error fetching movie list:", error);
+    return [];
+  }
+}
