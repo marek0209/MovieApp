d7b2dc6cc95e8b96f9432fcc528fcc6292492e64
marek0209 <marek.urbaniec1@gmail.com>
2023-05-21 23:36:28 +0200

    Move Button to separate component

--- src/elements/components/movie-list-item/MovieListItem.tsx
+++ src/elements/components/movie-list-item/MovieListItem.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Movie } from "../../../types";
 import resizeImage from "../../../utils/imageResizer";
 import ImageLoader from "../image-loader/ImageLoader";
-import Button from "../favorite-button/FavoriteButton";
+import FavoriteButton from "../favorite-button/FavoriteButton";
+import Button from "../button/Button";
 
 interface MovieListItemProps {
   movie: Movie;
@@ -39,26 +40,11 @@ export default function MovieListItem(props: MovieListItemProps) {
         </p>
         <div className="flex-grow"> </div>
         <div className="flex items-end">
-          <Button isFavorite={isFavorite} onClick={handleFavoriteClick} />
-          <a
-            href={movie.id.label}
-            className="ml-4 inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Watch on Itunes
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
+          <FavoriteButton
+            isFavorite={isFavorite}
+            onClick={handleFavoriteClick}
+          />
+          <Button href={movie.id.label}>Watch on Itunes</Button>
         </div>
       </div>
     </div>
